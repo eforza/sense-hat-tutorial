9d9755fab00bb8782461e51dc3d6417e046bb79a
eforza <eforzani@gmail.com>
2018-01-15 23:41:12 +0000

    Initial code.

---
+++ colour.py
@@ -0,0 +1,9 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+r = 255
+g = 0
+b = 0
+
+sense.clear((r, g, b))
---
+++ smile.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+sense.set_rotation(180)
+
+sense.set_pixel(2, 2, (0, 0, 255))
+sense.set_pixel(4, 2, (0, 0, 255))
+sense.set_pixel(3, 4, (100, 0, 0))
+sense.set_pixel(1, 5, (255, 0, 0))
+sense.set_pixel(2, 6, (255, 0, 0))
+sense.set_pixel(3, 6, (255, 0, 0))
+sense.set_pixel(4, 6, (255, 0, 0))
+sense.set_pixel(5, 5, (255, 0, 0))
+
+sleep(5)
+
+sense.clear()
